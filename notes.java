610d916613b61bfabcc47b03c59f113ac1bc593b
pratulyk <pratulya.k@gmail.com>
2016-08-02 13:03:23 +1000

    updating notes

---
+++ notes.java
@@ -0,0 +1,25 @@
+/*
+Notes for 2041 in java for formatting kek
+ */
+
+
+-l = Long in bash;
+
+//what is a file: Array of bytes. 0 or more bytes in a sequence
+//
+
+< = take the input from that file
+
+> = redirect the output to a file
+
+| = pipe symbol, standard output o a goes into b a|b
+
+strace = run this program and tell me what requests its making to the OS
+
+andrew is obsessed with cat 
+
+egrep = passes by default only some lines through. By default it is case sensitive;
+
+
+
+
